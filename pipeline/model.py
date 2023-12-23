adec1b5a24f204de2916ee618e237cc159b1842b
frwd-1 <frwdlabs@gmail.com>
2023-12-22 22:42:53 -0600

    Update model.py

--- pipeline/model.py
+++ pipeline/model.py
@@ -20,6 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = MultinomialNB()
 model.fit(X_train, y_train)
 
+# test
 # Evaluate the model
 predictions = model.predict(X_test)
 print(classification_report(y_test, predictions))
